0ec6a2696eae480502612c6b25cad5c92edf8bd5
“Sana <sanahejazirad@gmail.com>
2024-01-09 22:54:28 +0330

    Adding Project to git

---
+++ Sql&EntityFramework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sql_EntityFramework
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			database database =new database();
+			peopleEntities db = new peopleEntities();
+			//database.add("nika", "forghani",27);
+			//database.showdatabase();
+			//----------------------
+			var result2 = db.people.OrderByDescending(n => n.Age).ToArray();
+			foreach(var person in result2) { Console.WriteLine($"name : {person.Name} \n age :{person.Age} "); }
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Sql&EntityFramework/database.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sql_EntityFramework
+{
+	internal class database : people
+	{
+		peopleEntities db = new peopleEntities();
+		
+
+		public void add(String name, String family, int age)
+		{
+			bool run=false;
+			if (db.people.Any(n => n.Name == name && n.Family == family && n.Age == age))
+			{ run = true; }
+			if (run == false)
+			{
+				people p = new people();
+				p.Name = name;
+				p.Family = family;
+				p.Age = age;
+				db.people.Add(p);
+				db.SaveChanges();
+				Console.WriteLine("Contact Succeesfully Added...");
+
+			}
+			else { Console.WriteLine("This Contact exist..."); }
+		}
+
+			
+		public void remove(String name)
+		{
+
+			try
+			{
+				var remov = db.people.First(p => p.Name == name);
+				if (remov != null)
+				{
+					db.people.Remove(remov);
+					db.SaveChanges();
+					Console.WriteLine("contact removed");
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		public void editname(String Olename, String Newname)
+		{
+			var reult = db.people.SingleOrDefault(p => p.Name == Olename);
+			if (reult != null)
+			{
+				reult.Name = Newname;
+				db.SaveChanges();
+				Console.WriteLine("contact saved");
+			}
+			else
+			{
+				Console.WriteLine("Contact Not Found...");
+			}
+		}
+		public void editfamily(String name, String newfamily)
+		{
+			var reult = db.people.SingleOrDefault(p => p.Name == name);
+			if (reult != null)
+			{
+				reult.Family = newfamily;
+				db.SaveChanges();
+				Console.WriteLine("contact saved");
+			}
+			else
+			{
+				Console.WriteLine("Contact Not Found...");
+			}
+		}
+		public void editage(String name, int age)
+		{
+			var reult = db.people.SingleOrDefault(p => p.Name == name);
+			if (reult != null)
+			{
+				reult.Age = age;
+				db.SaveChanges();
+				Console.WriteLine("contact saved");
+			}
+			else
+			{
+				Console.WriteLine("Contact Not Found...");
+			}
+		}
+
+		public void showdatabase()
+		{
+			var result1 = db.people;
+			foreach (var sana in result1) { Console.WriteLine($"name : {sana.Name} family :{sana.Family} age :{sana.Age} \n"); }
+		}
+	}
+}
+
